b2bc2ffab10d9408b0c48bccabef804e9d11f179
saumyasurendiran <saumya@shortcine.com>
2014-10-26 19:27:14 +0530

    first commit

---
+++ film_controller.rb
@@ -0,0 +1,18 @@
+class FilmController < ApplicationController
+
+
+	def index
+
+		# render "hello"
+
+	end
+	def saumya
+		
+		@hello2="saumya	"
+		@hello="Varun"
+		render "hello"	
+
+	end
+
+
+end
